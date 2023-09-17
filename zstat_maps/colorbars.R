d8f2b667947381eec7c884530a0fcbc6f82f4e7a
alffajardo <faj.alf@gmail.com>
2023-09-17 14:22:38 -0400

    modifed colorbar

--- zstat_maps/colorbars.R
+++ zstat_maps/colorbars.R
@@ -8,13 +8,17 @@ library(RColorBrewer)
 
 # create red-yellow bar
 
-red_yellow <- colorRampPalette(colors = c("red","yellow"))
+red_yellow <- colorRampPalette(colors = c("red","yellow","white"))
 
 barplot(rep(1,1000), col = red_yellow(1000),border = red_yellow(1000),
         axes = F, main ="Zstat 1 (Sternberg Task Activation)",
          cex.main = 3)
 
+red_blue <- colorRampPalette(colors = c("red","white","blue"))
 
+barplot(rep(1,1000), col = red_blue(1000),border = red_blue(1000),
+        axes = F, main ="Zstat 1 (Sternberg Task Activation)",
+        cex.main = 3)
 ## plot the figure
 png("figS6_colorbar.png",width = 333,height = 205,units = "px")
 
@@ -23,12 +27,12 @@ par(mfrow = c(3,1),bg ="black",
 
 barplot(rep(1,1000), col = red_yellow(1000),border = red_yellow(1000),
         axes = F, main ="Zstat 1 (Sternberg Task Activation)",
-        cex.main = 1.5)
+        cex.main = 1)
 barplot(rep(1,1000), col = plasma(1000),border = plasma(1000),
-       axes = F, main ="GLM", cex.main = 1.5)
+       axes = F, main ="GLM", cex.main = 1)
 
 barplot(rep(1,1000), col = mako(1000),border = mako(1000),
-        axes = F, main = "ANCOVA", cex.main = 1.5)
+        axes = F, main = "ANCOVA", cex.main = 1)
 dev.off()
 
 
