989d0db54ef5f3c0a7a42fa371d7373dcf83a588
alffajardo <faj.alf@gmail.com>
2023-09-20 16:59:02 -0400

    finished interactive maps

--- zstat_maps/colorbars.R
+++ zstat_maps/colorbars.R
@@ -3,6 +3,18 @@
 library(viridis)
 library(magrittr)
 library(RColorBrewer)
+rgb2col = function(rgbmat){
+  # function to apply to each column of input rgbmat
+  ProcessColumn = function(col){
+    rgb(rgbmat[1, col], 
+        rgbmat[2, col], 
+        rgbmat[3, col], 
+        maxColorValue = 255)
+  }
+  # Apply the function
+  sapply(1:ncol(rgbmat), ProcessColumn)
+}
+
 
 # create the mako and the fire color bars
 
@@ -40,3 +52,34 @@ barplot(rep(1,1000), col = mako(1000),border = mako(1000),
 dev.off()
 
 
+# wintercolours bar
+
+winter_colors <- read.delim("colormaps/2winter.cmap",sep = " ",
+                            header = F)
+# sample 10 colors from the color bar
+
+winter_indexes <- seq(1,256,length.out = 10) %>%
+                  round(digits = 0)
+winter_10 <- winter_colors[winter_indexes,] %>%
+             as.matrix()
+
+write.table(cbind(seq(0,1,length.out = 10),winter_10),"winter.csv",col.names = F,
+            quote = F,row.names = F,sep = ",")
+winter_10_hex <- rgb2col(rgbmat = t(winter_10 * 255))
+# same for copper 
+
+copper <- read.table("colormaps/copper.cmap")
+
+copper_10 <- copper[c(winter_indexes[1:9],255),] %>%
+             as.matrix()
+copper_10_hex <- rgb2col(t(copper_10) * 255)
+
+barplot(1:10:1:10, col =  copper_10_hex)
+
+# write the colormap, 
+
+write.table(cbind(seq(0,1,length.out = 10),copper_10),
+            "copper.csv",
+            quote = F,sep = ",",
+            col.names = F,
+            row.names = F)
